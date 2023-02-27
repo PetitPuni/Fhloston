8ae307941d7ded502981eb75611e62e98d09644f
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-02-27 16:34:39 +0100

    Finished seeds for 50 planets and 2 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,31 @@
 require 'faker'
 
-puts 'DB start'
+Planet.destroy_all
+User.destroy_all
 
-5.times do
+puts 'DB start'
+user1 = User.create(
+  email: 'tata@gmail.com',
+  password: 'azerty'
+)
+
+user2 = User.create(
+  email: 'toto@gmail.com',
+  password: 'azerty'
+)
+
+50.times do
   Planet.create!(
     name: Faker::Space.star,
-    planet_type: ['Carbon planet', 'Gas dwarf', 'Desert planet', 'Gas giant', 'Helium planet', 'Ice giant', 'Iron planet', 'Lava planet'].sample
+    planet_type: ['Carbon planet', 'Gas dwarf', 'Desert planet', 'Gas giant', 'Helium planet', 'Ice giant', 'Iron planet', 'Lava planet'].sample,
+    summary: Faker::Restaurant.description,
+    inhabitants: [1..7_000_000_000].sample,
+    available_places: [1..5].sample,
+    location: Faker::Space.galaxy,
+    catch_phrase: Faker::Company.catch_phrase,
+    price: [1..20_000],
+    user: [user1, user2].sample
   )
 end
 
 puts 'DB finished'
-
-
-# t.string "planet_type"
-# t.string "summary"
-# t.integer "inhabitants"
-# t.integer "available_places"
-# t.string "location"
-# t.integer "price"
