0c88c53078fda425a9138110dca0ec815b2fa0a3
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-02-27 15:41:48 +0100

    Fixed Planet model with  Through bug

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,5 +1,5 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :users, through :booking
+  has_many :users, through: :booking
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-50 times do
+5.times do
   Planet.create(
-
+    name: Faker::Space.planet
   )
 end
