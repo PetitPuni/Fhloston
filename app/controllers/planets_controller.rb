133adbf052a5c9575719c97f19cdbc5755995228
jonathan rouas <rouasjonathan@gmail.com>
2023-02-27 16:35:54 +0100

    routes added to the controllers review/planets and to the routes.rb + modify update and destroy of planet

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -16,7 +16,7 @@ class PlanetsController < ApplicationController
     @planet = Planet.new(params_planet)
     @planet.user = @user
     if @planet.save
-      redirect_to #
+      redirect_to planet_path(@planet)
     else
       render new_planet_path, status: :unprocessable_entity
     end
@@ -26,9 +26,13 @@ class PlanetsController < ApplicationController
   end
 
   def update
+    @planet.update(params_planet)
+    redirect_to planet_path(@planet)
   end
 
   def delete
+    @planet.destroy
+    redirect_to planets_path
   end
 
   private
@@ -38,7 +42,7 @@ class PlanetsController < ApplicationController
   end
 
   def params_planet
-    params.require(:planet).permit(:name, :photo)
+    params.require(:planet).permit(:name, :photo, :planet_type, :summary, :inhabitants, :available_places, :location, :catch_phrase, :price)
   end
 
 end
