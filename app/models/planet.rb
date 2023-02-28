c83aa7b382f28973d195c55e1ff6e49fb65d26c9
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-02-28 11:20:27 +0100

    Added validations to user model (only for username)

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -13,12 +13,3 @@ class Planet < ApplicationRecord
   validates :location, presence: true
   validates :price, presence: true, numericality: { only_integer: true, in: [1..20_000] }
 end
-
-# t.string "planet_type" - drop-down @
-# t.string "summary" - minimum de 100 letters @
-# t.integer "inhabitants"  - réutiliser les ranges utiliser / [1..7_000_000_000] @
-# t.integer "available_places" - presence true, integer @
-# t.string "location" - presence true / drop-down avec quelques proposition @
-# t.integer "price" - réutiliser le range @
-# t.string "name" - presence true @
-# t.string "catch_phrase" - presence true / minimum 50 words @
