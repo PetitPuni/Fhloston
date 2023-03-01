d42da0df389d644a6e6973fc4cabf90bd651c527
jonathan rouas <rouasjonathan@gmail.com>
2023-03-01 22:51:49 +0100

    updated front for profile page and reservation page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_authenticity_token
+  rescue_from ActiveStorage::IntegrityError, with: :handle_integrity_error
 
   def show
   end
@@ -7,13 +9,27 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(params_user)
-    redirect_to user_path(current_user)
+    if current_user.update(params_user)
+      redirect_to user_path(current_user)
+    else
+      flash.now[:error] = "Failed to update user"
+      render :edit
+    end
   end
 
   private
 
   def params_user
     params.require(:user).permit(:photo, :username, :description)
+  end
+
+  def handle_invalid_authenticity_token
+    flash[:error] = "Stale request - please try again"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def handle_integrity_error
+    flash[:error] = "Failed to upload file - please try again"
+    redirect_back(fallback_location: root_path)
   end
 end
