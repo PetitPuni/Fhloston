92035867cee5ff3e23ddc4e85af60fe41e23c70a
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-02 12:28:06 +0100

    test commit but do not use it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authorize_user
 
   def show
-    current_user
   end
 
   def edit
-    current_user
   end
 
   def update
     current_user.update(params_user)
+    authorize current_user
     redirect_to user_path(current_user)
   end
 
@@ -17,5 +17,9 @@ class UsersController < ApplicationController
 
   def params_user
     params.require(:user).permit(:photo, :username, :description)
+  end
+
+  def authorize_user
+    authorize current_user
   end
 end
