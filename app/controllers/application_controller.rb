674e8e9206838a010a443c984c60d632d479e5da
Louis Charbonnier <charbonnierlouis@gmail.com>
2023-03-02 11:21:11 +0100

    delete2 pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 
-
-    private
-
-    def skip_pundit?
-      devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-    end
 end
